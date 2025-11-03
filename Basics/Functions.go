2662040b3aeb08ca31ebf012257c55d02c8cd3bf
Ankan Saha <ankansahadaluabari@gmail.com>
2025-11-03 23:43:59 +0530

    Learned Functions

---
+++ Basics/Functions.go
@@ -0,0 +1,17 @@
+package basics
+
+func FunctionSExample () {
+}
+
+
+func FunctionWithParams (a int, b int) int {
+	return  a + b;
+}
+
+func FunctionWithSameType (a, b int) int {
+	return a +b;
+}
+
+func FunctionWithMultipleReturn (a int, b int) (string, string) {
+	return "Ankan", "Saha"
+}
